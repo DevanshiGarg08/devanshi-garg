3d5ae8656ba98373920a3e2a245e6d6d94434498
Devanshi Garg <devanshig@geekyants.com>
2025-02-23 15:10:37 +0530

    changed a-> link

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import { usePathname } from "next/navigation"; // Import usePathname to get the current route
 
 export default function Navbar() {
@@ -7,32 +8,32 @@ export default function Navbar() {
   return (
     <nav className="absolute top-6 right-10 md:right-20 flex space-x-8 text-lg font-medium z-50">
       {pathname !== "/" && (
-        <a
+        <Link
           href="/"
           className="hover:text-gray-600 transition duration-300 font-poppins"
         >
           home
-        </a>
+        </Link>
       )}
 
-      <a
+      <Link
         href="/skills"
         className="hover:text-gray-600 transition duration-300 font-poppins"
       >
         skills
-      </a>
-      <a
+      </Link>
+      <Link
         href="/projects"
         className="hover:text-gray-600 transition duration-300 font-poppins"
       >
         projects
-      </a>
-      <a
+      </Link>
+      <Link
         href="/blogs"
         className="hover:text-gray-600 transition duration-300 font-poppins"
       >
         blogs
-      </a>
+      </Link>
     </nav>
   );
 }
